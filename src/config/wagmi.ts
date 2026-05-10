aac7668036d7bb25d81be43381f7727644e13c94
BradMyrick <55957519+BradMyrick@users.noreply.github.com>
2026-05-10 18:29:14 -0500

    Update WalletConnect project ID

--- src/config/wagmi.ts
+++ src/config/wagmi.ts
@@ -3,7 +3,7 @@ import { getDefaultConfig } from "@rainbow-me/rainbowkit";
 
 export const config = getDefaultConfig({
   appName: "Divechain",
-  projectId: "divechain-demo-app",
+  projectId: "70a3137c4b8369465907036fa8867c39",
   chains: [avalancheFuji, avalanche],
   ssr: true,
 });
